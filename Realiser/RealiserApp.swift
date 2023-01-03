be318c7fa97ba6c84400f303a20af765d8f959ee
Qingyun Yang <qyang063@uottawa.ca>
2023-01-03 00:22:50 -0500

    Initial Commit

---
+++ Realiser/RealiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealiserApp.swift
+//  Realiser
+//
+//  Created by Mikami Kirigaya on 2023-01-03.
+//
+
+import SwiftUI
+
+@main
+struct RealiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
